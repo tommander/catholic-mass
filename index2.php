8ebf33cfd068eb163db162a644bb714b5f6e7d73
rajnozka <rajnozka@gmail.com>
2022-09-26 20:25:46 +0200

    ongoing changes towards the first useful version

--- index2.php
+++ index2.php
@@ -1,44 +1,6 @@
-<?php 
-    class MassData {
-        public $jd;
-        public $tl;
-        public $ll;
-
-        function __construct() {
-            $this->jd = json_decode(file_get_contents('data.json'), true);
-            $this->tl = 'eng';
-            $this->ll = 'eng';
-
-            if (array_key_exists('ll', $_GET)) {
-                if (array_search($_GET['ll'], $this->jd['languages']) !== FALSE) {
-                    $this->ll = $_GET['ll'];
-                }
-            }
-            if (array_key_exists('tl',$_GET)) {
-                if (array_search($_GET['tl'], $this->jd['languages']) !== FALSE) {
-                    $this->tl = $_GET['tl'];
-                }
-            }
-        }
-
-        private function replcb(array $matches):string {
-            return $this->jd['labels'][$matches[1]][$this->ll];
-        }
-
-        public function repl($text) {
-            return preg_replace_callback('/@\{([A-Za-z0-9]+)\}/', 'self::replcb', $text);
-        }
-
-        public function link($label = '', $text = '') {
-            $putlabel = ($label != '') ? $label : $this->ll;
-            $puttext = ($text != '') ? $text : $this->tl;
-            return "index2.php?ll=${putlabel}&tl=${puttext}";
-        }
-    }
-
-    $md = new MassData();
-?>
 <?php
+	require('massdata.php');
+    $md = new MassData();
 ?>
 <!DOCTYPE html>
 <html lang="<?= $md->jd['languages'][$md->ll]['html']?>">
@@ -46,19 +8,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $md->repl('@{heading}') ?></title>
+    <title><?= $md->repls('@{heading}') ?></title>
     <link rel="stylesheet" href="fonts.css">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="fontawesome/css/all.css">
   </head>
   <body>
     <header>
-      <h1><?= $md->repl('@{heading}') ?></h1>
+      <h1><?= $md->repls('@{heading}') ?></h1>
     </header>
     <nav>
         <div>
-            <span><?= $md->repl('@{idxL}') ?></span>
-            <span><?= $md->repl('@{idxT}') ?></span>
+            <span><?= $md->repls('@{idxL}') ?></span>
+            <span><?= $md->repls('@{idxT}') ?></span>
         </div>
         <div>
             <div>
@@ -73,31 +35,24 @@
             </div>
         </div>
         <div>
-            <span>P = <?= $md->repl('@{lblP}') ?></span>
-            <span>A = <?= $md->repl('@{lblA}') ?></span>
-            <span>R = <?= $md->repl('@{lblR}') ?></span>
+            <span>P = <?= $md->repls('@{lblP}') ?></span>
+            <span>A = <?= $md->repls('@{lblA}') ?></span>
+            <span>R = <?= $md->repls('@{lblR}') ?></span>
+            <span>C = <?= $md->repls('@{lblC}') ?></span>
         </div>
     </nav>
     <main>
-        <?php foreach ($md->jd['texts'][$md->tl] as $one): ?>
-            <?php if (!is_array($one)): ?>
-                <div>Wut???</div>
-            <?php elseif (count($one) == 1): ?>
-            <?php foreach ($one as $k=>$v): ?>
-                <div>
-                    <!-- <?= var_export($one) ?> -->
-                    <span><?= $k ?>: </span>
-                    <span><?= $md->repl($v) ?></span>
-                </div>
-            <?php endforeach; ?>
-            <?php else: ?>
-                <div>
-                    wooohooo
-                </div>
-            <?php endif; ?>
-        <?php endforeach; ?>
+		<?= $md->html(); ?>
     </main>
     <footer>
+		<span><?= $md->repls('@{license}');?>: <a href="https://www.gnu.org/licenses/gpl-3.0.html">GNU GPL v3</a></span>
+        <span><?= $md->repls('@{source}');?>: <a href="https://github.com/tommander/catholic-mass">Repository at GitHub.com</a></span>
+        <span><?= $md->repls('@{author}');?>: <a href="mailto:tommander@tommander.cz">Tomáš <q>Tommander</q> Rajnoha</a></span>
+        <span>&nbsp;</span>
+        <span><?= $md->repls('@{headerimg}');?>: <a href="https://commons.wikimedia.org/wiki/File:Iglesia_de_San_Carlos_Borromeo,_Viena,_Austria,_2020-01-31,_DD_164-166_HDR.jpg">Iglesia de San Carlos Borromeo, Viena, Austria by Diego Delso</a> (<a href="https://creativecommons.org/licenses/by-sa/4.0">CC BY-SA 4.0</a>)</span>
+        <span><?= $md->repls('@{icons}');?>: <a href="https://fontawesome.com">Font Awesome Free 5.15.2 by @fontawesome</a> (<a href="https://fontawesome.com/license/free">Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License</a>)</span>
+        <span><?= $md->repls('@{font}');?>: <a href="https://fonts.google.com/specimen/Source+Sans+Pro">Source Sans Pro by Paul D. Hunt</a> (<a href="https://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=OFL">Open Fonts License</a>)</span>
+        <span><?= $md->repls('@{texts}');?>: <a href="https://www.catholicbishops.ie/wp-content/uploads/2011/02/Order-of-Mass.pdf">English</a>, <a href="https://www.cirkev.cz/cs/mse-svata">Čeština</a>, <a href="https://ourparishpriest.blogspot.com/2018/12/holy-mass-in-filipino-tagalog.html">Tagalog</a></span>
     </footer>
   </body>
 </html>
---
+++ massdata.php
@@ -0,0 +1,101 @@
+<?php 
+    class MassData {
+        public $jd;
+        public $tl;
+        public $ll;
+		private $icons = [
+			'cross' => 'fas fa-cross',
+			'bible' => 'fas fa-bible',
+			'bubble' => 'far fa-comment',
+			'peace' => 'far fa-handshake',
+			'walk' => 'fas fa-hiking',
+			'stand' => 'fas fa-male',
+			'sit' => 'fas fa-chair',
+			'kneel' => 'fas fa-pray'
+		];
+
+        function __construct() {
+            $this->jd = json_decode(file_get_contents('data.json'), true);
+            $this->tl = 'eng';
+            $this->ll = 'eng';
+
+            if (array_key_exists('ll', $_GET)) {
+				echo "<!-- WEOREG -->";
+                if (array_key_exists($_GET['ll'], $this->jd['languages']) !== FALSE) {
+					echo "<!-- GREGEG -->";
+                    $this->ll = $_GET['ll'];
+                }
+            }
+            if (array_key_exists('tl',$_GET)) {
+                if (array_key_exists($_GET['tl'], $this->jd['languages']) !== FALSE) {
+                    $this->tl = $_GET['tl'];
+                }
+            }
+        }
+
+        private function replcb(array $matches):string {
+            return "<span class=\"command\">".$this->jd['labels'][$matches[1]][$this->ll]."</span>";
+        }
+
+        private function replcbs(array $matches):string {
+            return $this->jd['labels'][$matches[1]][$this->ll];
+        }
+
+        private function replico(array $matches):string {
+            return "<i class=\"".$this->icons[$matches[1]]."\"></i>";
+        }
+
+        public function repl(string $text) {
+            return preg_replace_callback_array(['/@\{([A-Za-z0-9]+)\}/' => 'self::replcb', '/@icon\{([A-Za-z0-9]+)\}/' => 'self::replico'], $text);
+        }
+
+        public function repls(string $text) {
+            return preg_replace_callback_array(['/@\{([A-Za-z0-9]+)\}/' => 'self::replcbs', '/@icon\{([A-Za-z0-9]+)\}/' => 'self::replico'], $text);
+        }
+
+        public function link($label = '', $text = '') {
+            $putlabel = ($label != '') ? $label : $this->ll;
+            $puttext = ($text != '') ? $text : $this->tl;
+            return "index2.php?ll=${putlabel}&tl=${puttext}";
+        }
+
+		public function html() {
+			$ret = '';
+
+			foreach ($this->jd['texts'][$this->tl] as $one) {
+				if (!is_array($one)) {
+					continue;
+				}
+				
+				if (count($one) == 1) {
+					foreach($one as $k=>$v) {
+						$ret .= "<div><span class=\"who\">$k:</span><span class=\"what\">".$this->repl($v)."</span></div>\r\n";
+					}
+					continue;
+				}
+
+				$ret .= "<div class=\"options\">\r\n";
+				foreach ($one as $oneone) {
+					$ret .= "<div>\r\n";
+					if (count($oneone) == 1) {
+						foreach($oneone as $k=>$v) {
+							$ret .= "<div><span class=\"who\">$k:</span><span class=\"what\">".$v."</span></div>\r\n";
+						}
+					} else {
+						foreach ($oneone as $oneoneone) {
+							foreach($oneoneone as $k=>$v) {
+								$ret .= "<div><span class=\"who\">$k:</span><span class=\"what\">".$v."</span></div>\r\n";
+							}
+	
+						}
+					}
+					$ret .= "</div>\r\n";
+				}
+				$ret .= "</div>\r\n";
+			}
+
+			return $ret;
+		}
+
+    }
+?>
